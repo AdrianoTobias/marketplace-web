8dd02642ddebb052452de15117b8724f951c28a1
Adriano <adrianoact@msn.com>
2024-10-06 23:21:29 -0300

    add global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './global.css'
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold underline">Hello World!</h1>
+    <div>
+      <h1>Hello World!</h1>
     </div>
   )
 }
