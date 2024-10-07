e7cf87ebff921d777c3a1581f2b6331e6ffa9542
Adriano <adrianoact@msn.com>
2024-10-06 21:11:49 -0300

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './global.css'
+
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold underline">Hello World!</h1>
+    </div>
+  )
+}
