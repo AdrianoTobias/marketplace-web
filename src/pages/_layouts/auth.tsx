8feed2c218e4281ebc68e645593068be4e4b10f8
Adriano <adrianoact@msn.com>
2024-10-08 00:32:16 -0300

    implement auth layout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,22 @@
 import { Outlet } from 'react-router-dom'
 
+import backgroundImage from '../../assets/images/background.svg'
+import logoImage from '../../assets/images/logo.svg'
+
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="grid grid-cols-12">
+      <div className="col-span-7 flex h-full flex-col gap-[51px]">
+        <div className="pl-10 pt-10">
+          <img src={logoImage} alt="Logo" />
+        </div>
 
-      <div>
+        <div className="flex items-center justify-center">
+          <img src={backgroundImage} alt="Background image" />
+        </div>
+      </div>
+
+      <div className="col-span-5 flex h-[768px] items-center justify-center p-6">
         <Outlet />
       </div>
     </div>
