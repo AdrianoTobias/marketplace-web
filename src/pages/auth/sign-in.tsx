809f99b066099fdb23ec9f39b22c8c2d4478d0c9
Adriano <adrianoact@msn.com>
2024-10-08 23:31:50 -0300

    add Link on sign-in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import z from 'zod'
 
 import accessIcon from '../../assets/icons/access.svg'
@@ -36,7 +37,9 @@ export function SignIn() {
       <div className="h-full w-full rounded-[32px] bg-[var(--white)] px-20 py-[4.5rem]">
         <div className="flex h-full flex-col gap-12">
           <div className="flex flex-col gap-2 ">
-            <h2 className="title-md">Acesse sua conta</h2>
+            <h2 className="title-md text-[var(--gray-500)]">
+              Acesse sua conta
+            </h2>
             <p className="body-sm">Informe seu e-mail e senha para entrar</p>
           </div>
 
@@ -84,18 +87,23 @@ export function SignIn() {
             <p className="body-md text-[var(--gray-300)]">
               Ainda não tem uma conta?
             </p>
-            <button
-              className={`flex h-14 w-full items-center justify-between rounded-[.625rem] border-[1px] border-[var(--orange-base)] px-5 text-[var(--orange-base)] transition-colors duration-200                            
+
+            <div>
+              <Link to="/sign-up">
+                <button
+                  className={`flex h-14 w-full items-center justify-between rounded-[.625rem] border-[1px] border-[var(--orange-base)] px-5 text-[var(--orange-base)] transition-colors duration-200                            
                 ${isSubmitting ? 'cursor-not-allowed opacity-55' : 'hover:border-[var(--orange-dark)] hover:text-[var(--orange-dark)]'}`}
-              disabled={isSubmitting}
-            >
-              <span className="action-md">Cadastrar</span>
-              <img
-                src={arrowRightIconOrange}
-                className="h-6 w-6"
-                alt="Seta para a direita"
-              />
-            </button>
+                  disabled={isSubmitting}
+                >
+                  <span className="action-md">Cadastrar</span>
+                  <img
+                    src={arrowRightIconOrange}
+                    className="h-6 w-6"
+                    alt="Seta para a direita"
+                  />
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
