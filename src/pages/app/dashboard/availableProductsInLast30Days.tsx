cdd5b42f1dc9a453b2f3e129de8b5bdee9bc3873
Adriano <adrianoact@msn.com>
2024-10-12 17:03:46 -0300

    imlpement dashboard metric: available products in last 30 days

--- src/pages/app/dashboard/availableProductsInLast30Days.tsx
+++ src/pages/app/dashboard/availableProductsInLast30Days.tsx
@@ -1,6 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getAvailableProductsInLast30Days } from '../../../api/get-available-products-in-last-30-days'
 import storeIcon from '../../../assets//icons/store.svg'
 
 export function AvailableProductsInLast30Days() {
+  const { data: availableProducts } = useQuery({
+    queryKey: ['metrics', 'available-roducts-in-last-30-days'],
+    queryFn: getAvailableProductsInLast30Days,
+  })
+
   return (
     <div className="flex gap-4 rounded-[20px] bg-[var(--white)] pl-3 pr-7">
       <div className="my-3 flex h-[86px] w-20 items-center justify-center rounded-xl bg-[var(--blue-light)]">
@@ -8,7 +16,9 @@ export function AvailableProductsInLast30Days() {
       </div>
 
       <div className="flex flex-col justify-between py-5">
-        <h1 className="title-lg text-[var(--gray-400)]">56</h1>
+        <h1 className="title-lg text-[var(--gray-400)]">
+          {availableProducts?.amount}
+        </h1>
         <p className="body-xs text-[var(--gray-300)]">Produtos anunciados</p>
       </div>
     </div>
