fc1dd448024ce9747d67e81409b5c4daf72fed04
Adriano <adrianoact@msn.com>
2024-10-12 17:10:38 -0300

    implement dashboard metric: views received in last 30 days

--- src/pages/app/dashboard/viewsReceivedInLast30Days.tsx
+++ src/pages/app/dashboard/viewsReceivedInLast30Days.tsx
@@ -1,6 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getViewsReceivedInLast30Days } from '../../../api/views-received-in-last-30-days'
 import userMultipleIcon from '../../../assets//icons/user-multiple.svg'
 
 export function ViewsReceivedInLast30Days() {
+  const { data: viewsReceived } = useQuery({
+    queryKey: ['mretics', 'views-received-in-last-30-days'],
+    queryFn: getViewsReceivedInLast30Days,
+  })
+
   return (
     <div className="flex gap-4 rounded-[20px] bg-[var(--white)] pl-3 pr-7">
       <div className="my-3 flex h-[86px] w-20 items-center justify-center rounded-xl bg-[var(--blue-light)]">
@@ -12,7 +20,9 @@ export function ViewsReceivedInLast30Days() {
       </div>
 
       <div className="flex flex-col justify-between py-5">
-        <h1 className="title-lg text-[var(--gray-400)]">1.238</h1>
+        <h1 className="title-lg text-[var(--gray-400)]">
+          {viewsReceived?.amount}
+        </h1>
         <p className="body-xs text-[var(--gray-300)]">Pessoas visitantes</p>
       </div>
     </div>
