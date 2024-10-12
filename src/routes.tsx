f9835ab486f75748376dfcbf325229e450a72ad6
Adriano <adrianoact@msn.com>
2024-10-12 15:00:35 -0300

    add error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,11 +9,13 @@ import { EditProduct } from './pages/app/products/editProduct'
 import { Products } from './pages/app/products/products'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/products', element: <Products /> },
