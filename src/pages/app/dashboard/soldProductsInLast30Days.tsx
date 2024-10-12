ba19d83c08dfc20644c86e9496e8460ac7365e5f
Adriano <adrianoact@msn.com>
2024-10-12 16:58:00 -0300

    implement dashboard metric: sold products in last 30 days

--- src/pages/app/dashboard/soldProductsInLast30Days.tsx
+++ src/pages/app/dashboard/soldProductsInLast30Days.tsx
@@ -1,6 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getSoldProductsInLast30Days } from '../../../api/get-sold-products-in-last-30-days'
 import saleTagBlueIcon from '../../../assets//icons/sale-tag-blue.svg'
 
 export function SoldProductsInLast30Days() {
+  const { data: soldProducts } = useQuery({
+    queryKey: ['metrics', 'sold-products-in-last-30-days'],
+    queryFn: getSoldProductsInLast30Days,
+  })
+
   return (
     <div className="flex gap-4 rounded-[20px] bg-[var(--white)] pl-3 pr-7">
       <div className="my-3 flex h-[86px] w-20 items-center justify-center rounded-xl bg-[var(--blue-light)]">
@@ -12,7 +20,9 @@ export function SoldProductsInLast30Days() {
       </div>
 
       <div className="flex flex-col justify-between py-5">
-        <h1 className="title-lg text-[var(--gray-400)]">24</h1>
+        <h1 className="title-lg text-[var(--gray-400)]">
+          {soldProducts?.amount}
+        </h1>
         <p className="body-xs text-[var(--gray-300)]">Produtos vendidos</p>
       </div>
     </div>
