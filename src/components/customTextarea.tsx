dc85a1ed6db38a92961ba5b811e4e77d0cde3be1
Adriano <adrianoact@msn.com>
2024-11-06 23:17:56 -0300

    fix customTextarea className

--- src/components/customTextarea.tsx
+++ src/components/customTextarea.tsx
@@ -16,7 +16,7 @@ export function CustomTextarea({
     <div className="border-b-[1px] border-gray-100 px-0.5 py-3.5 focus-within:border-gray-400">
       <textarea
         placeholder={placeholder}
-        className="utline-none body-md w-full flex-1 text-gray-400 placeholder-gray-200 focus:outline-none"
+        className="w-full flex-1 text-gray-400 placeholder-gray-200 outline-none"
         {...props}
         {...register}
       />
